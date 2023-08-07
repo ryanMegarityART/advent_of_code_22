8f0c0cc31a8be545c2116ac4128ee5b5b1933486
Ryan Megarity <ryan.megarity@immerse.io>
2023-08-07 15:54:49 +0100

    Update Go timing to use prebuilt binary

--- day1/Go_attempt/timed_run.sh
+++ day1/Go_attempt/timed_run.sh
@@ -1,5 +1,5 @@
 start=$(date +%s%N)
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-go run $SCRIPT_DIR/$(dirname "$0")/main.go
+$SCRIPT_DIR/$(dirname "$0")/main
 end=$(date +%s%N)
 echo Execution time for day 1 Go_attempt was $(expr $end - $start) nanoseconds.
